1d983c7a2c5c8b4f1d5a52c64ff16b02289728f5
Yury Shapovalov <yurij.shapovalov@in-aim.ru>
2024-10-14 10:58:41 +0300

    fixed

--- shared/constants/auth-options.ts
+++ shared/constants/auth-options.ts
@@ -63,7 +63,7 @@ export const authOptions: AuthOptions = {
       },
     }),
   ],
-  // secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: 'jwt',
   },
